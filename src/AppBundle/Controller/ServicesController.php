25baa7922f54fcc49c4a1e554c95cf13bae25051
Lunkos <kuster.lunuel@yahoo.com>
2019-05-05 16:26:44 +0200

    Documentation
    init doc

--- src/AppBundle/Controller/ServicesController.php
+++ src/AppBundle/Controller/ServicesController.php
@@ -21,8 +21,12 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 
 class ServicesController extends Controller
 {
+
+
     /**
      * @Route("/", name="services")
+
+     * index
      */
     public function indexAction(Request $request)
     {
@@ -34,15 +38,18 @@ class ServicesController extends Controller
 
     /**
      * @Route("/Visiteur/{login}/{pass}", name="jsonvisiteur_service_byloginpass")
+     *
+     * Obtenir un visiteur par son identifiant et mot de passe
+     * @var login, pass
+     * @return JsonResponse 
+     *
      **/
     public function jsonVisiteurByLogpassAction(Request $request,$login, $pass)
     {
         $repository = $this->getDoctrine()->getManager()->getRepository('AppBundle:Visiteur');
         $user = $repository->findOneBy(array('visLogin' => $login,'visMdp' => $pass));
 
-        if (!$user) {
-            return new JsonResponse();
-        }
+        if (!$user) {return new JsonResponse();}
 
         $data = array();
 
@@ -60,6 +67,10 @@ class ServicesController extends Controller
 
     /**
      * @Route("/Visiteur/{matricule}", name="jsonvisiteur_service_byId")
+     * Obtenir un visiteur par son matricule
+     * @var matricule
+     * @return JsonResponse
+     * 
      */
     public function jsonVisiteurByIdAction(Request $request,$matricule)
     {
@@ -90,6 +101,9 @@ class ServicesController extends Controller
 
     /**
      * @Route("/Visiteur/", name="jsonvisiteur_service_All")
+     * Obtenir tous les visiteurs
+     * @var request
+     * @return JsonResponse 
      */
     public function jsonVisiteurAction(Request $request)
     {
@@ -119,6 +133,9 @@ class ServicesController extends Controller
 
     /**
      * @Route("/Visiteur/{matricule}/Rapports/", name="rapports_ByMatricule")
+     * Obtenir les rapports de visites d'un visiteur par son matricule
+     * @var matricule
+     * @return JsonResponse 
      */
     public function getRapportsAction(Request $request,$matricule)
     {
@@ -156,6 +173,9 @@ class ServicesController extends Controller
 
     /**
      * @Route("/Visiteur/{matricule}/Rapport/{id_Rapport}", name="rapports_ByMatriculeIdRapport")
+     * Obtenir un rapport de visite par le matricule d'un visiteur et l'id d'un rapport
+     * @var matricule, idRapport
+     * @return JsonResponse 
      */
     public function getRapportAction(Request $request,$matricule,$id_Rapport)
     {
@@ -188,6 +208,9 @@ class ServicesController extends Controller
 
     /**
      * @Route("/Visiteur/{matricule}/Praticiens/", name="praticien_ByMatricule")
+     * Obtenir les praticiens en rapport avec le matricule d'un visiteur 
+     * @var matricule
+     * @return JsonResponse 
      */
     public function getPraticiensAction(Request $request,$matricule)
     {
@@ -227,6 +250,9 @@ class ServicesController extends Controller
 
     /**
      * @Route("/Visiteur/add/rapport/", name="add_rapport")
+     * Ajouter un rapport de visite
+     * @var request
+     * @return object
      */
     public function AjouterRapportAction(Request $request)
     {
@@ -277,7 +303,6 @@ class ServicesController extends Controller
         $em->flush();
         
         die;
-
 
     }
 }
